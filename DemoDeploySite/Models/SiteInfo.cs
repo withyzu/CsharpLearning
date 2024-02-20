097f5941c251727912a19661445a7b232a2a4b75
Helong Zhang <withyzu@outlook.com>
2024-02-20 21:20:03 +0800

    modfiy

---
+++ DemoDeploySite/Models/SiteInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoDeploySite.Models;
+public class SiteInfo {
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = "不试试你怎么知道~ 😋";
+
+    public int Start { get; set; }
+
+    public int ClickCount { get; set; }
+
+}
--- DemoSToCs/DemoSToCs/Data/ApplicationDbContext.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace DemoSToCs.Data {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
-    }
-}
